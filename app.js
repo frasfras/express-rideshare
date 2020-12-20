cdd15e18cbda6213d341d56024aaa3ad35e42420
frasfras <65541080+frasfras@users.noreply.github.com>
2020-12-21 06:44:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+const testData = {
+  name: 'John Doe',
+  age: '30',
+  account_balance: '15.5'
+}
+
 app.get("/", (req, res) => res.send("Hello from Render!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
